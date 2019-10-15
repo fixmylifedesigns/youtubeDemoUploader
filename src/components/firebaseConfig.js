5be9be252a3d5dcebeed8c963ba128141f14bc13
fixmylifedesigns <ijd.irving@gmail.com>
2019-10-15 02:18:08 -0400

    apis

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBEEJW6vyYxTYMY-6FCZF-STJB-DP3AvF4",
-  authDomain: "demouploader-71303.firebaseapp.com",
-  databaseURL: "https://demouploader-71303.firebaseio.com",
-  projectId: "demouploader-71303",
-  storageBucket: "demouploader-71303.appspot.com",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "460092740979",
-  appId: "1:460092740979:web:7550c2a15f2c515e65a106",
-  measurementId: "G-6XNBLCTJW8"
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
